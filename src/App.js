2e84564d6afee0b8f52a9b2198bd9fc5faa526d4
Charles Fourdrignier <charles@teracode.fr>
2018-06-15 17:13:02 +0200

    WIP - Banner

--- src/App.js
+++ src/App.js
@@ -16,10 +16,15 @@ const state = poneys;
 class App extends Component {
   render() {
     return (
-      <div className="flex">
-        <PolyComponent state={state} />
-        <PolyComponent state={update(state)} />
-      </div>
+      <React.Fragment>
+        <div className="overflow-hidden w-100 bg-blue-lighter my-1 mx-2 p-2 text-grey-darker text-2xl scroll-right">
+          <p>hello, world</p>
+        </div>
+        <div className="flex">
+          <PolyComponent state={state} />
+          <PolyComponent state={update(state)} />
+        </div>
+      </React.Fragment>
     );
   }
 }
