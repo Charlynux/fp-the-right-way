5ea2886a1899a0aea5ab8ab2da94dd60fcb3eb81
Charles Fourdrignier <charles@teracode.fr>
2018-07-10 12:02:36 +0200

    Change prices

--- src/data.js
+++ src/data.js
@@ -1,12 +1,16 @@
 export const simplePoney = {
-  name: "Twilight Starla Sparkle",
-  image: "images/twilight.jpg",
+  name:
+    "Twilight Starla Sparkle",
+  image:
+    "images/twilight.jpg",
   price: 3300
 };
 
 export const complexPoney = {
-  name: "Twilight Starla Sparkle",
-  image: "images/twilight.jpg",
+  name:
+    "Twilight Starla Sparkle",
+  image:
+    "images/twilight.jpg",
   meta: {
     prices: {
       price: 3300,
@@ -18,20 +22,24 @@ export const complexPoney = {
 export const poneys = [
   {
     id: 1,
-    name: "Twilight Starla Sparkle",
-    image: "images/twilight.jpg",
+    name:
+      "Twilight Starla Sparkle",
+    image:
+      "images/twilight.jpg",
     meta: {
       new: false,
       prices: {
-        price: 3300,
-        premiumPrice: 3900
+        price: 2700,
+        premiumPrice: 2900
       }
     }
   },
   {
     id: 2,
-    name: "Sarah Blossom Apple",
-    image: "images/blossom.jpg",
+    name:
+      "Sarah Blossom Apple",
+    image:
+      "images/blossom.jpg",
     meta: {
       new: false,
       prices: {
@@ -43,54 +51,67 @@ export const poneys = [
   {
     id: 3,
     name: "Juliet Ann Rarity",
-    image: "images/rarity.jpg",
+    image:
+      "images/rarity.jpg",
     meta: {
       new: true,
       prices: {
-        price: 3300,
-        premiumPrice: 3900
+        price: 1600,
+        premiumPrice: 1700
       }
     }
   },
   {
     id: 4,
-    name: "Barbara Roxane Seed",
+    name:
+      "Barbara Roxane Seed",
     image: "images/seed.jpg",
     meta: {
       new: false,
       prices: {
-        price: 3300,
-        premiumPrice: 3900
+        price: 2100,
+        premiumPrice: 2700
       }
     }
   },
   {
     id: 5,
     name: "Rainbow Dash",
-    image: "images/mc-dashin.png",
+    image:
+      "images/mc-dashin.png",
     meta: {
       new: false,
       prices: {
-        price: 3300,
-        premiumPrice: 3900
+        price: 4500,
+        premiumPrice: 4800
       }
     }
   },
   {
     id: 6,
-    name: "Emilé Melody Fluttershy",
-    image: "images/melody.jpg",
+    name:
+      "Emilé Melody Fluttershy",
+    image:
+      "images/melody.jpg",
     meta: {
       new: false,
       prices: {
-        price: 3300,
-        premiumPrice: 3900
+        price: 3800,
+        premiumPrice: 4900
       }
     }
   }
 ];
 
 export const exampleBannerItems = [
-  { label: "iPhone X en promo !!!", price: 3000 },
-  { label: "Le Wiko, c'est pour le boulot", price: 100 }
+  {
+    label:
+      "iPhone X en promo !!!",
+    price: 3000
+  },
+  {
+    label:
+      "Le Wiko, c'est pour le boulot",
+    price: 100
+  }
 ];
