470e3733038f87a658d290135d1e5b5387d1f2c5
Charles Fourdrignier <charles@teracode.fr>
2018-07-10 19:36:28 +0200

    After the talk

--- src/App.js
+++ src/App.js
@@ -23,35 +23,90 @@ import {
   applySuperDiscount
 } from "./utils";
 
-const update = function(state) {
-  return state;
-};
+// const getPrice = state =>
+//   state.meta.prices.price;
+// const setPrice = (newPrice, state) => ({
+//   ...state,
+//   meta: {
+//     ...state.meta,
+//     prices: {
+//       ...state.meta.prices,
+//       price: newPrice
+//     }
+//   }
+// });
+
+// const lensPrice = R.lens(
+//   getPrice,
+//   setPrice
+// );
+const lensPoneyPrice = [
+  "meta",
+  "prices",
+  L.defaults({
+    premiumPrice: 500
+  }),
+  "price",
+  L.defaults(2000)
+];
+const lensPrice = [
+  // L.first,
+  // L.last,
+  // L.find(poney => !poney.meta.new),
+  L.elems,
+  // L.when(poney => !poney.meta.new),
+  lensPoneyPrice
+];
+
+//Redux: state, action => state
+const update = state =>
+  L.modify(
+    lensPrice,
+    applySuperDiscount,
+    state
+  );
+
+const createBannerItem = R.applySpec({
+  label: poney => poney.name,
+  price: L.get(lensPoneyPrice)
+});
+
+// poneys => poneys => items
+const prepareBannerItems = R.compose(
+  R.map(createBannerItem),
+  R.take(2),
+  R.sortBy(L.get(lensPoneyPrice)),
+  R.filter(L.get(["meta", "new"]))
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      name: "Twilight Starla Sparkle",
-      image: "images/twilight.jpg",
-      price: 3300
-    };
+    this.state = poneys;
   }
 
   render() {
     return (
       <React.Fragment>
-        {/* <PricesBanner items={exampleBannerItems} /> */}
+        <PricesBanner
+          items={prepareBannerItems(
+            poneys
+          )}
+        />
         <div className="flex">
           <PolyComponent
             state={this.state}
           />
+          <PolyComponent
+            state={update(this.state)}
+          />
         </div>
-        <PrettyButton
+        {/* <PrettyButton
           onClick={() =>
             this.setState(update)
           }
-        />
+        /> */}
       </React.Fragment>
     );
   }
